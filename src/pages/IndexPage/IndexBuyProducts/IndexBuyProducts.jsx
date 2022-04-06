6c1d3cfcf3bb107cfe05f7f2295ff1785fbf42a8
knucklesuganda <testaddressss43@gmail.com>
2022-04-06 16:07:17 +0200

    Started working with web3modal

--- src/pages/IndexPage/IndexBuyProducts/IndexBuyProducts.jsx
+++ src/pages/IndexPage/IndexBuyProducts/IndexBuyProducts.jsx
@@ -1,16 +1,17 @@
-import { useState, useEffect, Fragment } from "react";
-import { Row, Col, Card, message, Button } from "antd";
+import { useState, Fragment } from "react";
+import { Row, Col, Card, message, Button, Space, Typography } from "antd";
 import { BuyModal } from "../../../components/BuyModal";
 import { Title } from "../../../components/Title";
-import { connectWeb3Account } from "../../../web3";
+import { connectWallet } from "../../../web3/wallet/providers";
 import './style.css';
 
 
 function ProductCard(props) {
-    return <Col key={props.index} span={4} style={{ cursor: "pointer" }}>
-        <Card title={props.title} className="productCard"
-            extra={<img style={{ width: 50 }} alt={props.title} src={props.image} />}
-            onClick={props.handleClick}>{props.description}</Card>
+    return <Col key={props.index} span={4} style={props.style}>
+        <Card title={props.title} onClick={props.handleClick} className={props.className} extra={<img
+            style={{ width: 50 }} alt={props.title} src={props.image} />}>
+            {props.description}
+        </Card>
     </Col>
 }
 
@@ -20,46 +21,75 @@ export function IndexBuyProducts(props) {
     const [account, setAccount] = useState(null);
     const [productAddress, setProductAddress] = useState(null);
 
+    const placeholderProducts = [];
+
+    for (let i = 0; i < 18; i++) {
+        placeholderProducts.push(<ProductCard
+            index={i} title="Product image"
+            style={{ cursor: "inherit" }}
+            image="https://picsum.photos/200"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh vel tortor."
+        />);
+    }
+
     return <Row>
         <Title id={props.id}>Buy products</Title>
 
-        {account === null ? <Row style={{ display: "flex" }}>
-            {[1, 2, 3, 4, 5].map((index) =>
-                <ProductCard
-                    index={index}
-                    image="https://i.picsum.photos/id/1005/200/300.jpg"
-                    title="Product image"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh vel tortor."
-                />
-            )}
+        {account === null ? <Row gutter={[25, 55]} style={{
+            display: "flex", paddingLeft: "1em", rowGap: "10px", columnGap: "10px"
+        }}>
+            <Row style={{ width: "100%", zIndex: -1, filter: "blur(4px)" }}>{placeholderProducts}</Row>
 
-            <Button type="primary" onClick={() => {
-                connectWeb3Account().then((account) => {
-                    setAccount(account);
-                }).catch((error) => {
-                    message.error({
-                        content: error.toString(),
-                        duration: 5,
-                    });
-                });
-            }}>Connect account</Button>
-        </Row>
-        :
-        <Fragment>
-            <Row gutter={[16, 16]} style={{ paddingTop: "2em", paddingLeft: "1em", paddingRight: "1em" }}>
-                {[1, 2, 3, 4, 5].map((index) =>
-                    <ProductCard
-                        title="Meta index"
-                        productImage="https://i.picsum.photos/id/1005/200/300.jpg"
-                        handleClick={() => {
-                            setIsBuyOpen(true);
-                            setProductAddress('0x725CA50819AD2353d69311f7b090ED1541d3e443');
-                        }}
-                    />
-                )}
+            <Row style={{
+                position: "absolute",
+                zIndex: 10,
+                left: "40%", right: "50%",
+                top: "20%", bottom: "50%",
+                marginLeft: "auto",
+                marginRight: "auto",
+                textAlign: "center",
+            }}>
+                <Card style={{
+                    boxShadow: "3px 3px 84px 0px rgba(255, 255, 255, 0.2)"
+                }}>
+                    <Typography.Text style={{ fontSize: "1.2em" }}>
+                        You must connect your wallet in order to buy products
+                    </Typography.Text>
+
+                    <Button type="primary" size="large" style={{
+                        width: "20em", marginTop: "1em",
+                    }} 
+                    onClick={() => {
+                        connectWallet().then((account) => {
+                            setAccount(account);
+                        }).catch((error) => {
+                            message.error({
+                                content: error.toString(),
+                                duration: 5,
+                            });
+                        });
+                    }}>Connect account</Button>
+                </Card>
             </Row>
+        </Row> :
+            <Fragment>
+                <Row gutter={[16, 16]} style={{ paddingTop: "2em", paddingLeft: "1em", paddingRight: "1em" }}>
+                    {[1, 2, 3, 4, 5].map((index) =>
+                        <ProductCard
+                            index={index}
+                            title="Meta index"
+                            className="productCard"
+                            productImage="https://i.picsum.photos/id/1005/200/300.jpg"
+                            handleClick={() => {
+                                setIsBuyOpen(true);
+                                setProductAddress('0x725CA50819AD2353d69311f7b090ED1541d3e443');
+                            }}
+                            style={{ cursor: "pointer" }}
+                        />
+                    )}
+                </Row>
 
-            <BuyModal account={props.account} productAddress={productAddress} state={[isBuyOpen, setIsBuyOpen]} />
-        </Fragment>}
+                <BuyModal account={props.account} productAddress={productAddress} state={[isBuyOpen, setIsBuyOpen]} />
+            </Fragment>}
     </Row>;
 }
--- src/web3/wallet/providers.js
+++ src/web3/wallet/providers.js
@@ -1,17 +1,34 @@
-import { getEthereum } from "./functions";
+import Web3Modal from "web3modal";
 import { ethers } from "ethers";
+import WalletConnectProvider from '@walletconnect/web3-provider';
 
 
-const ethereum = getEthereum();
-let provider, signer;
+let web3Modal;
 
-if(!ethereum){
-    provider = null;
-    signer = null;
-}else{
-    provider = new ethers.providers.Web3Provider(ethereum);
+const providerOptions = {
+    walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+            rpc: {
+                1337: process.env.PUBLIC_RPC_URL,
+            }
+        },
+    }
+};
+
+
+export async function connectWallet(){
+    web3Modal = new Web3Modal({
+        cacheProvider: false,
+        providerOptions,
+    });
+
+    const web3ModalProvider = await web3Modal.connect();
+
+    provider = new ethers.providers.Web3Provider(web3ModalProvider);
     signer = provider.getSigner();
 }
 
 
-export { provider, signer };
+let signer = null, provider = null;
+export { signer, provider };
