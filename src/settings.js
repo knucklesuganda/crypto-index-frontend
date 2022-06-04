6dd5539b42e36bb8bd6a124bf3ccefab95769b96
knucklesuganda <testaddressss43@gmail.com>
2022-06-04 21:30:07 +0200

    Settings fix

--- src/settings.js
+++ src/settings.js
@@ -1,10 +1,12 @@
 
+console.log(process.env);
 const settings = {
-    OBSERVER_ADDRESS: process.env.OBSERVER_ADDRESS,
-    STATE_UPDATE_INTERVAL: parseInt(process.env.STATE_UPDATE_INTERVAL),
-    PUBLIC_RPC_URL: process.env.PUBLIC_RPC_URL,
-    DEBUG: process.env.NODE_ENV === 'development',
-    CHAIN_ID: process.env.CHAIN_ID,
+    OBSERVER_ADDRESS: '',
+    STATE_UPDATE_INTERVAL: parseInt(10000),
+    PUBLIC_RPC_URL: 'https://eth-mainnet.alchemyapi.io/v2/KQmEoXn9kKfPL4AAljiW1XV39HRHff_K',
+    DEBUG: false,
+    CHAIN_ID: '0x1',
 };
+console.log(settings.PUBLIC_RPC_URL)
 
 export default settings;
