2e565dace652e14e7e55e34478466bf7e211bd28
knucklesuganda <testaddressss43@gmail.com>
2022-06-22 07:18:13 +0200

    Fixed settings

--- src/settings.js
+++ src/settings.js
@@ -1,8 +1,8 @@
 
 const settings = {
-    OBSERVER_ADDRESS: '0x7212569605978ce4cc26489611df873706fbc2a1', //'0x00d7ae7a188c88f365a9F2eEEb6a4028660eda42',
+    OBSERVER_ADDRESS: '0x00d7ae7a188c88f365a9F2eEEb6a4028660eda42',
     STATE_UPDATE_INTERVAL: parseInt(10000),
-    PUBLIC_RPC_URL: 'http://localhost:8545', // 'https://eth-mainnet.alchemyapi.io/v2/KQmEoXn9kKfPL4AAljiW1XV39HRHff_K',
+    PUBLIC_RPC_URL: 'https://eth-mainnet.alchemyapi.io/v2/KQmEoXn9kKfPL4AAljiW1XV39HRHff_K',
     DEBUG: false,
     CHAIN_ID: '0x1',
     BUY_DAI_LINK: 'https://app.uniswap.org/#/swap?'
