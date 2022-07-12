014f1472c1f979f09cd382e7f79944a2b83b1ee4
knucklesuganda <testaddressss43@gmail.com>
2022-07-12 21:48:16 +0600

    Improved image sizes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import "./App.css";
 function App() {
     document.title = "Void";
 
-    return <Suspense fallback={null}>
+    return <Suspense fallback={"Loading"}>
         <BrowserRouter>
             <Header>
                 <Routes>
