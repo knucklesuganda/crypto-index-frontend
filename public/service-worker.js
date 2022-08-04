1a78334930a26fe4dc8abe79280db0c618453cf7
knucklesuganda <testaddressss43@gmail.com>
2022-08-04 11:54:22 +0600

    Updated cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'void_management_cache';
+const CACHE_NAME = 'void_management_storage';
 
 self.addEventListener("activate", event => {
     event.waitUntil(caches.keys().then(keyList =>
@@ -6,28 +6,25 @@ self.addEventListener("activate", event => {
             if (key !== CACHE_NAME) {
                 return caches.delete(key);
             }
-        })))
-    );
+        }))
+    ));
 });
 
 
 self.addEventListener('install', function (event) {
-    event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
-        cache.addAll(['/']);
-    }));
+    event.waitUntil(caches.open(CACHE_NAME).then(function (cache) { cache.addAll(['/']); }));
 });
 
 self.addEventListener('fetch', function (event) {
 
     event.respondWith((async () => {
         const cacheMatch = await caches.match(event.request);
-        if(cacheMatch){
+        if (cacheMatch) {
             return cacheMatch;
         }
 
         // if (cacheMatch && cacheMatch.headers.has('Date')) {
         //     const cacheDate = new Date(cacheMatch.headers.has('Date'));
-// 
         //     if (Date.now() > cacheDate.getTime() + 60000 * 1) {
         //         return cacheMatch;
         //     }
